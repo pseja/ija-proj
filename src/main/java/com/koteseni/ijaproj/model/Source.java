a8567ab000d38d0437ac86ddd71794cc4c29c212
Lukas Pseja <luk4sps3j4@gmail.com>
2025-04-26 19:17:37 +0200

    power source implementation

--- src/main/java/com/koteseni/ijaproj/model/Source.java
+++ src/main/java/com/koteseni/ijaproj/model/Source.java
@@ -1,5 +1,18 @@
 package com.koteseni.ijaproj.model;
 
-public class Source {
+import java.util.EnumSet;
 
+public class Source extends Tile {
+
+    public Source(int row, int col, EnumSet<Direction> connections) {
+        super(row, col, connections);
+        this.connected = true;
+    }
+
+    @Override
+    public void setConnected(boolean connected) {
+        // always set this to true just in case I try to set it to false somewhere on
+        // accident
+        this.connected = true;
+    }
 }
