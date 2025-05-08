31f88a486dba4af252338c7ad5a651be58f5a1cf
Lukas Pseja <luk4sps3j4@gmail.com>
2025-05-08 21:41:56 +0200

    wire javadoc

--- src/main/java/com/koteseni/ijaproj/model/Wire.java
+++ src/main/java/com/koteseni/ijaproj/model/Wire.java
@@ -2,21 +2,44 @@ package com.koteseni.ijaproj.model;
 
 import java.util.EnumSet;
 
+/**
+ * Represents a wire tile on the game board.
+ * 
+ * @author ≽^•⩊•^≼ The Koteseni Team ≽^•⩊•^≼
+ */
 public class Wire extends Tile {
 
+    /** The wire shape of this wire. */
     private final WireShape shape;
 
+    /**
+     * Creates a new Wire at the specified position with the given shape.
+     *
+     * @param row   Row position of the wire
+     * @param col   Column position of the wire
+     * @param shape The wire shape defining the wire's connections
+     */
     public Wire(int row, int col, WireShape shape) {
         super(row, col, EnumSet.noneOf(Direction.class));
         setConnections(shape.createConnections());
         this.shape = shape;
     }
 
+    /**
+     * Sets the power state of this wire.
+     * 
+     * @param powered true to power the wire, false to unpower it
+     */
     @Override
     public void setPowered(boolean powered) {
         this.powered = powered;
     }
 
+    /**
+     * Gets the shape of this wire.
+     *
+     * @return The shape of the wire
+     */
     public WireShape getShape() {
         return shape;
     }
