50bc0aaf632fbd91cb6c9b13b94ba2806c1dfd16
Lukas Pseja <luk4sps3j4@gmail.com>
2025-05-08 17:40:11 +0200

    scene controller javadoc

--- src/main/java/com/koteseni/ijaproj/controller/SceneController.java
+++ src/main/java/com/koteseni/ijaproj/controller/SceneController.java
@@ -9,8 +9,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Controller for handling seamless scene transitions.
+ * 
+ * @author ≽^•⩊•^≼ The Koteseni Team ≽^•⩊•^≼
+ */
 public class SceneController {
 
+    /**
+     * Seamlessly changes the current scene to a new one.
+     *
+     * @param scene_name The window title
+     * @param fxml_path  The path to the FXML file where the new scene is defined
+     * @param stage      The stage to update with the new scene
+     * 
+     * @return The FXMLLoader used to load the scene
+     * 
+     * @throws IOException For errors loading the FXML file
+     */
     public static FXMLLoader changeScene(String scene_name, String fxml_path, Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxml_path));
         Parent root = loader.load();
