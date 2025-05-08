391ce15fa9a0424f672f8ebfd311542e6129b8fd
Lukas Pseja <luk4sps3j4@gmail.com>
2025-05-08 23:32:08 +0200

    plugins for packing the project into a jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,17 @@
+// Module definition for the game
+// Authors: Lukas Pseja (xpsejal00), Vaclav Sovak (xsovakv00)
+
 module com.koteseni.ijaproj {
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
     requires transitive javafx.web;
 
-    requires transitive gson;
+    requires gson;
     requires java.sql;
 
     opens com.koteseni.ijaproj to javafx.fxml;
     opens com.koteseni.ijaproj.controller to javafx.fxml;
-    opens com.koteseni.ijaproj.model to javafx.fxml;
+    opens com.koteseni.ijaproj.model to javafx.fxml, gson;
     opens com.koteseni.ijaproj.view to javafx.fxml;
 
     exports com.koteseni.ijaproj;
