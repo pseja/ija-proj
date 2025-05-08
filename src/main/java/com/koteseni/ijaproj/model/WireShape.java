59e3918063002c4fd9c55a53ae6c36413eabe8e7
Lukas Pseja <luk4sps3j4@gmail.com>
2025-05-08 21:45:42 +0200

    wire shape javadoc

--- src/main/java/com/koteseni/ijaproj/model/WireShape.java
+++ src/main/java/com/koteseni/ijaproj/model/WireShape.java
@@ -2,13 +2,30 @@ package com.koteseni.ijaproj.model;
 
 import java.util.EnumSet;
 
+/**
+ * Enumeration of the possible wire shapes.
+ * 
+ * @author ≽^•⩊•^≼ The Koteseni Team ≽^•⩊•^≼
+ */
 public enum WireShape {
 
+    /** Straight wire e.g. NORTH to SOUTH */
     I,
+
+    /** T wire with connections in three directions, e.g. NORTH, EAST and SOUTH */
     T,
+
+    /** Corner wire with two connections, e.g. NORTH and EAST */
     L,
+
+    /** Cross wire with connections in all four directions */
     X;
 
+    /**
+     * Creates a set of directions based on this wire shape.
+     * 
+     * @return Set of directions in which this shape has connections
+     */
     public EnumSet<Direction> createConnections() {
         EnumSet<Direction> connections = EnumSet.noneOf(Direction.class);
 
@@ -37,6 +54,13 @@ public enum WireShape {
         return connections;
     }
 
+    /**
+     * Picks the appropriate wire shape based on a set of connections.
+     * 
+     * @param connections The set of directions in which connections exist
+     * 
+     * @return The wire shape that matches the given connection pattern
+     */
     public static WireShape fromConnections(EnumSet<Direction> connections) {
         return switch (connections.size()) {
             case 2 -> {
